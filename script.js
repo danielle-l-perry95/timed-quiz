3c4dbf6c9a118fe08bb5f8acf6e64d2d3eb6fb30
Danielle Perry <nsbd95@gmail.com>
2022-11-11 15:31:31 -0500

    changed .textcontent to .innerHTML for the timer

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ startBtn.addEventListener("click",function() {
         questionContainer.style.display = "none"
     }
 })
-// set the timer to start on startbtn click. Needs fixing 
+// set the timer to start on startbtn click. How to fix stopping the alert and going back to main page when OK is clicked. 
 document.getElementById("start-btn").addEventListener("click",function() {
     var timeRemaining = 30
     setInterval(function(){
-        document.getElementById("timer-seconds").textContent = timeRemaining--
+        document.getElementById("timer-seconds").innerHTML = timeRemaining-- + " seconds"
         if (timeRemaining <= 0) {
             clearInterval(timeRemaining)
             alert(`Quiz Failed! You ran out of time.`)
         }
-    },1000)
+    }, 1000)
 },)
 

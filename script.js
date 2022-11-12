18127faca6428f35e6c43b1b0f87473b1395a1ef
Danielle Perry <nsbd95@gmail.com>
2022-11-12 10:22:04 -0500

    added on click event for when the correct answer is clicked for Q2, Q3 shows up

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ correctAnswerOneBtn.addEventListener("click",function() {
     }
 })
 
+var correctAnswerTwoBtn = document.getElementById("btn-answer-two")
+var questionContainerThree = document.getElementById("question-container-three")
+correctAnswerTwoBtn.addEventListener("click",function() {
+    if (questionContainerThree.style.display === "none") {
+        questionContainerThree.style.display = "block"
+    } else {
+        questionContainerThree.style.display = "none"
+    }
+})
+
 
 
 // set the timer to start on startbtn click. How to fix stopping the alert and going back to main page when OK is clicked. 
@@ -36,30 +46,30 @@ correctAnswerOneBtn.addEventListener("click",function() {
 // displays question two when correct answer is clicked
 
 // create objects for the questions 
-var questions = [
-    { 
-        title:"Question One",
-        question: "What does JSON stand for?",
-        answer: "Javascript object notation",
-        choices: [ "Javascript operating numbers","Javascript object notation","Javascript object numberator","Javascript open navigation"]
-    },
-    {
-        title: "Question Two",
-        question: "What is a variable?",
-        answer: "containers for storing data",
-        choices: ["containers for storing data","independent and dependent","a function","a for loop"]
-    },
-    {
-        title: "Question Three",
-        question: "An array is surrounded by what?",
-        answer: `[]`,
-        choices: [`{}`,`()`,`[]`,`""`]
-    },
-    {
-        title: "Question Four",
-        question: "What are three ways to declare a variable?",
-        answer: "let, const, and var",
-        choices: ["a, b, and c","let, const, and var","x, y, and z", "dependent and independent"]
-    }
-]
+// var questions = [
+//     { 
+//         title:"Question One",
+//         question: "What does JSON stand for?",
+//         answer: "Javascript object notation",
+//         choices: [ "Javascript operating numbers","Javascript object notation","Javascript object numberator","Javascript open navigation"]
+//     },
+//     {
+//         title: "Question Two",
+//         question: "What is a variable?",
+//         answer: "containers for storing data",
+//         choices: ["containers for storing data","independent and dependent","a function","a for loop"]
+//     },
+//     {
+//         title: "Question Three",
+//         question: "An array is surrounded by what?",
+//         answer: `[]`,
+//         choices: [`{}`,`()`,`[]`,`""`]
+//     },
+//     {
+//         title: "Question Four",
+//         question: "What are three ways to declare a variable?",
+//         answer: "let, const, and var",
+//         choices: ["a, b, and c","let, const, and var","x, y, and z", "dependent and independent"]
+//     }
+// ]
 

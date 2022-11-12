8d65bac38c5e03199203a33db03f1a61f5bb673f
Danielle Perry <nsbd95@gmail.com>
2022-11-12 11:44:08 -0500

    added onclick event to decrement timer and decrement score when wrongBtn is clicked. Also inc score when correctbtn is clicked

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
-// Shows question one after clicking start btn 
+// Declare variables globally 
 var startBtn = document.querySelector("#start-btn")
 var questionContainer = document.querySelector("#question-container")
+var wrongBtn = document.querySelector(".btn-wrong")
+// Shows question one after clicking start btn 
 startBtn.addEventListener("click",function() {
     if (questionContainer.style.display === "none") {
         questionContainer.style.display = "block"
@@ -9,7 +11,7 @@ startBtn.addEventListener("click",function() {
         questionContainer.style.display = "none"
     }
 })
-
+// shows question two afte clicking the correct answer 
 var correctAnswerOneBtn = document.getElementById("btn-answer-one")
 var questionContainerTwo = document.getElementById("question-container-two")
 correctAnswerOneBtn.addEventListener("click",function() {
@@ -19,7 +21,7 @@ correctAnswerOneBtn.addEventListener("click",function() {
         questionContainerTwo.style.display = "none"
     }
 })
-
+// shows question three after clicking the correct answer 
 var correctAnswerTwoBtn = document.getElementById("btn-answer-two")
 var questionContainerThree = document.getElementById("question-container-three")
 correctAnswerTwoBtn.addEventListener("click",function() {
@@ -29,7 +31,7 @@ correctAnswerTwoBtn.addEventListener("click",function() {
         questionContainerThree.style.display = "none"
     }
 })
-
+// shows question four after clicking the correct answer 
 var correctAnswerThreeBtn = document.getElementById("btn-answer-three")
 var questionContainerFour = document.getElementById("question-container-four")
 correctAnswerThreeBtn.addEventListener("click",function() {
@@ -41,45 +43,33 @@ correctAnswerThreeBtn.addEventListener("click",function() {
 })
 
 
+// set the timer to start on startbtn click. 
+document.getElementById("start-btn").addEventListener("click",function() {
+    var timeRemaining = 45
+    setInterval(function(){
+        document.getElementById("timer-seconds").innerHTML = timeRemaining-- + " seconds"
+        if (timeRemaining <= 0) {
+            clearInterval(timeRemaining)
+            alert(`Quiz Failed! You ran out of time.`)
+        }
+    }, 1000)
+    // This does not currently work. Need to figure out how to decrement time on click event 
+    // wrongBtn.addEventListener("click",function() {
+    //     timeRemaining -= 5
+    //     document.getElementById("timer-seconds").innerHTML = timeRemaining-- + " seconds"
+    // })
+},)
 
-// set the timer to start on startbtn click. How to fix stopping the alert and going back to main page when OK is clicked. 
-// document.getElementById("start-btn").addEventListener("click",function() {
-//     var timeRemaining = 30
-//     setInterval(function(){
-//         document.getElementById("timer-seconds").innerHTML = timeRemaining-- + " seconds"
-//         if (timeRemaining <= 0) {
-//             clearInterval(timeRemaining)
-//             alert(`Quiz Failed! You ran out of time.`)
-//         }
-//     }, 1000)
-// },)
-// displays question two when correct answer is clicked
+// Decrease score when the wrong btn is clicked. How to decrement timer more than one second. 
+var scoreNumber = document.getElementById("score-number")
+var results = 100
+var timeRemaining = 45
+function incorrect() {
+    document.getElementById("score-number").innerHTML = results--
+    document.getElementById("timer-seconds").innerHTML = timeRemaining-- + " seconds"
+}
 
-// create objects for the questions 
-// var questions = [
-//     { 
-//         title:"Question One",
-//         question: "What does JSON stand for?",
-//         answer: "Javascript object notation",
-//         choices: [ "Javascript operating numbers","Javascript object notation","Javascript object numberator","Javascript open navigation"]
-//     },
-//     {
-//         title: "Question Two",
-//         question: "What is a variable?",
-//         answer: "containers for storing data",
-//         choices: ["containers for storing data","independent and dependent","a function","a for loop"]
-//     },
-//     {
-//         title: "Question Three",
-//         question: "An array is surrounded by what?",
-//         answer: `[]`,
-//         choices: [`{}`,`()`,`[]`,`""`]
-//     },
-//     {
-//         title: "Question Four",
-//         question: "What are three ways to declare a variable?",
-//         answer: "let, const, and var",
-//         choices: ["a, b, and c","let, const, and var","x, y, and z", "dependent and independent"]
-//     }
-// ]
-
+// Increase score when correct btn is clicked 
+function correct() {
+    document.getElementById("score-number").innerHTML = results ++
+}
